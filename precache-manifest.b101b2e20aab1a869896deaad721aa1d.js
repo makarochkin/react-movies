f51fce1cf713f67b8e80b834177aa1577b5ad1e8
makarochkin <makarochkin.a.v@gmail.com>
2023-05-16 21:13:51 +0600

    Updates

---
+++ precache-manifest.b101b2e20aab1a869896deaad721aa1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17ed6fa5cbd1133ead7a",
+    "url": "/react-movies/static/css/main.91faf152.chunk.css"
+  },
+  {
+    "revision": "17ed6fa5cbd1133ead7a",
+    "url": "/react-movies/static/js/main.17ed6fa5.chunk.js"
+  },
+  {
+    "revision": "022280761d4275f8b4b5",
+    "url": "/react-movies/static/js/1.02228076.chunk.js"
+  },
+  {
+    "revision": "6e0964696bb83cd24dee",
+    "url": "/react-movies/static/js/runtime~main.6e096469.js"
+  },
+  {
+    "revision": "a7fc857c7440fdd0affc9bee32d4e340",
+    "url": "/react-movies/index.html"
+  }
+];
